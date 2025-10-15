8c93b1675f10deef4a4c8e170dbe798bcb301de8
Cesar Segura <cesar.segura@ucem.ac.cr>
2025-10-15 10:19:13 -0600

    Menu y Hamburger Icon

--- contacto.php
+++ contacto.php
@@ -1,5 +1,5 @@
 <?php require_once "./vistas/vista_superior.php"?>
 
-Esta es la página principal.
+Contacto
 
 <?php require_once "./vistas/vista_inferior.php"?>
--- xbox.php
+++ xbox.php
@@ -1,5 +1,5 @@
 <?php require_once "./vistas/vista_superior.php"?>
 
-Clientes
+Xbox todas las versiones
 
 <?php require_once "./vistas/vista_inferior.php"?>
